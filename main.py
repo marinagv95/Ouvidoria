42978bad754c95d96b12955a60ffe32c6600c859
Gabriela Wanderley <gabimoraisw@gmail.com>
2025-04-04 11:51:45 -0300

    Criação dos metodos e inicio do main

---
+++ main.py
@@ -0,0 +1,7 @@
+from operacoesdb import *
+
+conn = criarConexao('127.0.0.1', 'root', 'Sky@2024','ouvidoria')
+
+
+
+encerrarConexao(conn)
---
+++ metodos/operacoes_ouvidoria.py
@@ -0,0 +1,41 @@
+from operacoesdb import *
+
+def registrarManifestacao(conn, dados):
+    registrarmanifestacaoSQL = 'insert into manifestacao(descricao, autor, ouvidor, tipo) values(%s,%s,%s,%s,%s)'
+    registrar = insertNoBancoDados(conn, registrarmanifestacaoSQL, dados)
+
+    return registrar
+
+def listarManifestacoes(conn):
+    listarManifestacoesSQL= 'select * from manifestacao'
+    manifestacoes = listarBancoDados(conn, listarManifestacoesSQL)
+
+    return manifestacoes
+
+def listarQuantidadeDeManifestacoes(conn):
+    listarQuantidadeSQL = 'select count(*) from manifestacao'
+    quantidade = listarBancoDados(conn, listarQuantidadeSQL)
+
+    return quantidade
+
+
+def buscarManifestacaoPorCodigo(conn, dados):
+    buscarManifestacaoCodigoSQL = 'select * from manifestacao where codigo = %s'
+    codigoManifestacao = listarBancoDados(conn, buscarManifestacaoCodigoSQL, dados)
+
+    return codigoManifestacao
+
+
+def buscarManifestacaoTipo(conn, dados):
+    buscarManifestacaoTipoSQL = 'select tipo from manifestacao where codigo = %s'
+    tipo = listarBancoDados(conn, buscarManifestacaoTipoSQL, dados)
+
+    return tipo
+
+
+def removerManifestacaoPorCodigo(conn, dados):
+    removerManifestacaoSQL = 'delete from manifestacao where codigo = %s'
+    excluirManifestacao = excluirBancoDados(conn, removerManifestacaoSQL, dados)
+
+    return excluirManifestacao
+
