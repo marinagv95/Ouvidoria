adb101d70a6291fc181f77d5fcce14d4be792996
Marina Guimarães <mama_guima04@hotmail.com>
2025-04-04 19:16:00 -0300

    Criando a função de criarManifestacao

---
+++ operacoes_menu/criar_manifestacao.py
@@ -0,0 +1,20 @@
+from metodos.operacoes_ouvidoria import registrarManifestacao
+
+
+def criarManifestacao(conn):
+    while True:
+        descricao = input("Descrição da Manifestação: ")
+        autor = input("Autor da Manifestação: ")
+        ouvidor = input("Ouvidor da Manifestação: ")
+        tipo = input("Tipo da Manifestação (Reclamação, Sugestão, Elogio): ").lower()
+        dados = [descricao, autor, ouvidor, tipo]
+
+        if descricao == "" or autor == "" or ouvidor == "" or tipo == "":
+            print(f"Dados inválidos!")
+        elif 'reclamação' in tipo or 'sugestão' in tipo or 'elogio' in tipo:
+            registroManifestacao = registrarManifestacao(conn, dados)
+            print(f"Manifestação registrada com sucesso! - Código: {registroManifestacao} " )
+            break
+        elif 'reclamação' not in tipo or 'sugestão' not in tipo or 'elogio' not in tipo:
+            print(f"O tipo precisa ser: Reclamação, Sugestão, Elogio")
+
